e1e6c4bcc88a7f2539a531499752a9e5095d85b3
Jack Davison <45171616+jack-davison@users.noreply.github.com>
2022-12-03 11:11:04 +0000

    Update 2022_day03_solution.R

--- 2022/day03/2022_day03_solution.R
+++ 2022/day03/2022_day03_solution.R
@@ -6,28 +6,34 @@ scores <-
   tibble(letter = c(letters, LETTERS)) %>%
   mutate(score = row_number())
 
+# read input
 raw <- tibble(x = read_lines("2022/day03/2022_day03_input.txt"))
-
 
 # PART 1 ------------------------------------------------------------------
 
-# split into two backpacks
+# split into two compartments
 splits <-
   raw %>%
+  # split into two compartments
   mutate(len_compartment = str_length(x)/2,
          comp1 = str_sub(x, 0, len_compartment),
          comp2 = str_sub(x, len_compartment+1, -1)) %>%
   select(-x, -len_compartment) %>%
+  # get backpack number (useful for checking)
   mutate(backpack = row_number(), .before = everything())
 
 # get shared letters
 shared_letters <-
   splits %>%
+  # split into individual strings
   mutate(comp1 = str_split(comp1, ""),
          comp2 = str_split(comp2, "")) %>%
+  # put each letter on one row
   unnest_longer(comp1) %>%
   unnest_longer(comp2) %>%
+  # get matching letters
   filter(comp1 == comp2) %>%
+  # unique rows
   distinct()
 
 # find score
@@ -42,19 +48,25 @@ shared_letters %>%
 # assign groups / backpacks
 assigned_groups <-
   raw %>%
+  # assign groups using modulus of 3
   mutate(group = if_else(row_number() %% 3 == 0, row_number(), NA_integer_)) %>%
   fill(group, .direction = "up") %>%
+  # get backpack number per group
   group_by(group) %>%
   mutate(backpack = row_number())
 
-# rearrange data
+# count letters
 letter_counts <-
   assigned_groups %>%
+  # split and unnest (as in Part 1)
   mutate(x = str_split(x, "")) %>%
   unnest_longer(x) %>%
   ungroup() %>%
+  # count to get unique letter-group-backpack combos
   count(x, group, backpack) %>%
+  # put each backpack in a different column
   pivot_wider(names_from = backpack, values_from = n) %>%
+  # drop missing to get letters in every backpack
   drop_na()
 
 # find score
